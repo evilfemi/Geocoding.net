f3b2854b5f76ff19e3e04bf172a35e84cb1bb5d1
Femi Alashi <femisimon@github.com>
2016-10-10 16:51:40 -0700

    This is also a test

--- AssemblyInfoCommon.cs
+++ AssemblyInfoCommon.cs
@@ -38,4 +38,7 @@ Console.WriteLine('This line is used to test');
 Console.WriteLine('This line is used to test');
 ///This is another line of comment
 ///Console.WriteLine('This line is used to test')
+Console.WriteLine('This line is used to test');
+///This is another line of comment
+///Console.WriteLine('This line is used to test')
 Console.WriteLine('This line is used to test')'))
