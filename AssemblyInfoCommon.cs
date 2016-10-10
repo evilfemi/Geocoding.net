a84048aa9ac1db328c21d009a1cd60e146629b47
Femi Alashi <femisimon@github.com>
2016-10-10 13:31:15 -0700

    This is also a test

--- AssemblyInfoCommon.cs
+++ AssemblyInfoCommon.cs
@@ -8,4 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 Console.WriteLine('This line is used to test');
 ///This is another line of comment
-///Console.WriteLine('This line is used to test')
+///Console.WriteLine('This line is used to test
+///Console.WriteLine('This line is used to test');
+//////This is another line of comment
+Console.WriteLine('This line is used to test')')
