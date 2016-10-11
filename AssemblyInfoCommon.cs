93c70e69acfbd344ddfed126bdc47299b78f69dc
evilfemi <evilfemisimon@gmail.com>
2016-10-10 17:45:12 -0700

    make it test

--- AssemblyInfoCommon.cs
+++ AssemblyInfoCommon.cs
@@ -4,6 +4,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Geocoding.Net")]
 /// This is a test line
 /// another change
+/// /
+/// Make it rain
 [assembly: AssemblyCompany("Geocoding.Net")]
 [assembly: AssemblyCopyright("Copyright © 2008 - 2014 Chad Lee")]
 [assembly: ComVisible(false)]
